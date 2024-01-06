977cfc11b04fcd96d5c856e86be6afb876dc2e82
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-01-06 12:50:02 -0300

    fix: list all beers controller

--- src/main/java/com/egon/msscbeerinventoryservice/controllers/ListAllBeersByIdController.java
+++ src/main/java/com/egon/msscbeerinventoryservice/controllers/ListAllBeersByIdController.java
@@ -4,18 +4,22 @@ import com.egon.msscbeerinventoryservice.dtos.BeerInventoryDto;
 import com.egon.msscbeerinventoryservice.services.ListAllBeersByIdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("beers")
 @RequiredArgsConstructor
 public class ListAllBeersByIdController {
 
   private final ListAllBeersByIdService service;
 
+  @GetMapping("/{id}/inventory")
   public ResponseEntity<List<BeerInventoryDto>> listAllBeersById(
       @PathVariable UUID id) {
     return ResponseEntity.ok(service.execute(id));
